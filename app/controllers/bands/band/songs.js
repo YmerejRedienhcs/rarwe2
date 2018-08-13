e9f433b9e2e472369728a4ff6ce32869880a7964
Jeremy Schneider <jschneider@esri.com>
2018-08-13 16:33:40 -0400

    chapter 8 page 98

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,7 +1,14 @@
 // app/controllers/bands/band/songs.js
+import { isEmpty } from '@ember/utils';
+import { computed } from '@ember/object';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  name: '',
+
+  isAddButtonDisabled: computed('title', function() {
+    return isEmpty(this.get('title'));
+  }),
   actions: {
     updateRating: function(params) {
       var song = params.item,
