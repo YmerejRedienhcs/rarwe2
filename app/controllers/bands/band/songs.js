3500b6a44fd09e9a89d2c3d6a4b1d8e808fc2ca1
Jeremy Schneider <jschneider@esri.com>
2018-08-13 17:34:47 -0400

    changes from chapter 8

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -4,12 +4,19 @@ import { computed } from '@ember/object';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  songCreationStarted: false,
   name: '',
 
   isAddButtonDisabled: computed('title', function() {
     return isEmpty(this.get('title'));
   }),
+  canCreateSong: computed('songCreationStarted', 'model.songs.[]', function() {
+    return this.get('songCreationStarted') || this.get('model.songs.length');
+  }),
   actions: {
+    enableSongCreation: function() {
+      this.set('songCreationStarted', true);
+    },
     updateRating: function(params) {
       var song = params.item,
           rating = params.rating;
