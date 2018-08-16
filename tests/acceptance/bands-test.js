a1e8f65f5b3ef1756bc17889caf0a3e62b781c76
Jeremy Schneider <jschneider@esri.com>
2018-08-16 11:36:24 -0400

    chapter 11 page 167 -- add acceptance tests for listing bands and creating new band

---
+++ tests/acceptance/bands-test.js
@@ -0,0 +1,133 @@
+// tests/acceptance/bands-test.js
+import { test } from 'qunit';
+import moduleForAcceptance from 'rarwe/tests/helpers/module-for-acceptance';
+import Pretender from 'pretender';
+
+
+moduleForAcceptance('Acceptance | bands', {
+  afterEach() {
+    server.shutdown();
+  }
+});
+
+var server;
+
+test('List bands', function(assert) {
+  server = new Pretender(function() {
+    this.get('/bands', function() {
+      var response = {
+        data: [
+          {
+            id: 1,
+            type: 'bands',
+            attributes: {
+              name: 'Radiohead'
+            }
+          },
+          {
+            id: 2,
+            type: 'bands',
+            attributes: {
+              name: 'Long Distance Calling'
+            }
+          },
+        ]
+      };
+      return [200, { 'Content-Type': 'application/vnd.api+json' }, JSON.stringify(response)];
+    });
+  });
+
+  visit('/bands');
+
+  andThen(function() {
+    assert.equal(
+      currentURL(),
+      '/bands',
+      'The URL is correct after visiting /bands'
+    );
+    assert.equal(
+      find('.band-link').length,
+      2,
+      'All band links are rendered');
+    assert.equal(
+      find('.band-link:contains("Radiohead")').length,
+      1,
+      'First band link contains the band name');
+    assert.equal(
+      find('.band-link:contains("Long Distance Calling")').length,
+      1, 'The other band link contains the band name');
+  });
+});
+
+test('create a new band', function(assert) {
+  server = new Pretender(function() {
+    this.get('/bands', function() {
+      var response = {
+        data: [
+          {
+            id: 1,
+            type: 'bands',
+            attributes: {
+              name: 'Radiohead'
+            }
+          }
+        ]
+      };
+      return [
+        200,
+        {
+          'Content-Type': 'application/vnd.api+json'
+        },
+        JSON.stringify(response)
+      ];
+    });
+
+    this.post('/bands', function() {
+      var response = {
+        data: {
+          id: 2,
+          type: 'bands',
+          attributes: {
+            name: 'Long Distance Calling'
+          }
+        }
+      };
+      return [
+        200,
+        {
+          'Content-Type': 'application/vmd.api+json'
+        },
+        JSON.stringify(response)
+      ];
+    });
+
+    this.get('/bands/2/songs', function() {
+      var response = {
+        data: []
+      };
+      return [200, { 'Content-Type': 'application/vmd.api+json' }, JSON.stringify(response)];
+    });
+  });
+
+  visit('/bands');
+  fillIn('.new-band', 'Long Distance Calling');
+  click('.new-band-button');
+
+  andThen (function() {
+    assert.equal (
+      find('.band-link').length,
+      2,
+      'All band links are rendered'
+    );
+    assert.equal (
+      find('.band-link:last').text().trim(),
+      'Long Distance Calling',
+      'Created band appears at the end of the list'
+    );
+    assert.equal (
+      find('.nav a.active:contains("Songs")').length,
+      1,
+      'The Songs tab is active'
+    );
+  });
+});
