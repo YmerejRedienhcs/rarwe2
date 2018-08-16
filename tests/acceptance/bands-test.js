e987ac3f5c5c6f864dcac876b3f50ee1431161c6
Jeremy Schneider <jschneider@esri.com>
2018-08-16 11:56:24 -0400

    chapter 11 page 170 -- add acceptance test for adding a song

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -131,3 +131,60 @@ test('create a new band', function(assert) {
     );
   });
 });
+
+test('Create a new song in two steps', function(assert) {
+  server = new Pretender(function() {
+    this.get('/bands', function() {
+      var response = {
+        data: [
+          {
+            id: 1,
+            type: 'bands',
+            attributes: {
+              name: 'Radiohead'
+            }
+          }
+        ]
+      };
+      return [200, { 'Content-Type': 'applictation/vnd.api+json' }, JSON.stringify(response)];
+    });
+    this.get('/bands/1', function() {
+      var response = {
+        data: {
+          id: 1,
+          type: 'bands',
+          attributes: {
+            name: 'Radiohead'
+          }
+        }
+      };
+      return [200, { 'Content-Type': 'application/vnd.api+json'}, JSON.stringify(response)];
+    });
+    this.post('/songs', function() {
+      var response = {
+        data: {
+          id: 1,
+          type: 'songs',
+          attributes: {
+            nmae: 'Killer Cars'
+          }
+        }
+      };
+      return [200, { 'Content-Type': 'application/vnd.api+json' }, JSON.stringify(response)];
+    });
+  });
+
+  visit('/');
+  click('.band-link:contains("Radiohead")');
+  click('a:contains("create one")');
+  fillIn('.new-song', 'Killer Cars');
+  triggerEvent('.new-song-form', 'submit');
+
+  andThen(function() {
+    assert.equal(
+      find('.songs .song:contains("Killer Cars")').length,
+      1,
+      'Creates the song and displays it in the list'
+    );
+  });
+});
