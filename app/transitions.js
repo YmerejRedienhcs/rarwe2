aa9ccbb60fac852c886468d536b5230c89601877
Jeremy Schneider <jschneider@esri.com>
2018-09-27 13:57:44 -0400

    chapter 15 page 243 - transition from details to songs - slide right/left

---
+++ app/transitions.js
@@ -0,0 +1,9 @@
+// app/transitions.js
+export default function(){
+    this.transition(
+      this.fromRoute('bands.band.songs'),
+      this.toRoute('bands.band.details'),
+      this.use('toRight'),
+      this.reverse('toLeft')
+    );
+}
