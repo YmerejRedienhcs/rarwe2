63b23936ac136552958f3e327cc0dee237552c6c
Jeremy Schneider <jschneider@esri.com>
2018-09-27 16:24:11 -0400

    this.debug() logs to console showing why transition was not performed (class was misspelled as band-descripton)

--- app/transitions.js
+++ app/transitions.js
@@ -9,7 +9,8 @@ export default function(){
     this.transition(
       this.hasClass('band-description'),
       this.toValue(false),
-      this.use('fade', { duration: 500 })
+      this.use('fade', { duration: 500 }),
+      this.debug()
     );
     this.transition(
       this.inHelper('liquid-bind'),
